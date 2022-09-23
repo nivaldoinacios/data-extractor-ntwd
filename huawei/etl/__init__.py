2cc93ef83eb7a9ae2962c710c03fe0e693f086d2
inacionivaldo <nivaldo.lima@iesb.edu.br>
2022-09-23 11:14:20 -0300

    Funções ETL.

--- huawei/etl/__init__.py
+++ huawei/etl/__init__.py
@@ -12,5 +12,17 @@ df = df.rename(columns={'IPADDRESS_x': 'IPADDRESS'})
 df = pd.merge(df, df_statistics, how='outer', on=['MAC'])
 df = df.fillna(0)
 
+df.to_csv(
+    path_or_buf=os.getenv('dados'),
+    sep=';',
+    columns=['UserID', 'Username', 'IPADDRESS', 'MAC', 'Status', 'AP_ID',
+             'AP_NAME', 'RF/WLAN', 'BAND', 'Type', 'RX/TX', 'RSSI', 'VLAN',
+             'SSID', '@timestamp-py', "PCKT_SENT", "PCKT_RECEIVED", "BYTES_SENT",
+             "BYTES_RECEIVED", "WID_RATE_SENT", "WID_RATE_RECEIVED",
+             "TRIGGER_R_TOTAL", "TRIGGER_R_FAILED", "BYTES_SENT_CALC"],
+    index_label='index',
+    line_terminator='\n'
+)
+
 if __name__ == '__main__':
     print(df)
